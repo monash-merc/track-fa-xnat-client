dd15709cceb8abcf9a649fd4e4b3095a7c9a1c2c
manishkumr <rishimanish123@gmail.com>
2021-02-13 23:28:36 +1100

    set up eslint

--- utils/fetch_data.js
+++ utils/fetch_data.js
@@ -1,98 +1,107 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
 module.exports = {
-    authenticate_user: async (username, password, host) => {
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                Authorization: 'Basic ' + Buffer.from(username + ":" + password).toString('base64')
-            },
-            redirect: 'follow'
-        };
-        try{
-            const response = await  fetch(`${host}data/JSESSION`, requestOptions);
-            return await response.text();
-        } catch (error ){
-            console.log(error)
-        }
-    },
-    get_all_projects: async (cookie, host) => {
-         var requestOptions = {
-          method: 'GET',
-          headers: {
-              cookie: "JSESSIONID="+cookie
-          },
-          redirect: 'follow'
-        };
-         try{
-             const response = await fetch(`${host}data/projects?format=json`, requestOptions)
-             return await response.json();
-         } catch (error) {
-             console.log(error)
-         }
-    },
-    check_subjects: async (cookie, host, subject) => {
-         var requestOptions = {
-          method: 'GET',
-          headers: {
-              cookie: "JSESSIONID="+cookie
-          },
-          redirect: 'follow'
-        };
-         try{
-             const response = await fetch(`${host}data/projects/TRACKFA/subjects/${subject}?format=json`, requestOptions)
-             //console.log(response.json())
-             return !!response.ok;
-         } catch (error) {
-             console.log(error)
-         }
-    },
-    check_experiments: async (cookie, host, exp, subject) => {
-         var requestOptions = {
-          method: 'GET',
-          headers: {
-              cookie: "JSESSIONID="+cookie
-          },
-          redirect: 'follow'
-        };
-         try{
-             const response = await fetch(`${host}data/projects/TRACKFA/subjects/${subject}/experiments/${exp}?format=json`, requestOptions)
-             //console.log(response.json())
-             return !!response.ok;
-         } catch (error) {
-             console.log(error)
-         }
-    },
-    create_experiment: async (cookie, host, exp, subject, datatype) => {
-         var requestOptions = {
-          method: 'PUT',
-          headers: {
-              cookie: "JSESSIONID="+cookie
-          },
-          redirect: 'follow'
-        };
-         try{
-             const response = await fetch(`${host}data/projects/TRACKFA/subjects/${subject}/experiments/${exp}?xnat:${datatype}/acquisition_site=Monash`, requestOptions)
-             //console.log(response.json())
-             return !!response.ok;
-         } catch (error) {
-             console.log(error)
-         }
-    },
-    add_resource: async (cookie, host, exp, subject, datatype, file) => {
-         var requestOptions = {
-          method: 'PUT',
-          headers: {
-              cookie: "JSESSIONID="+cookie
-          },
-          redirect: 'follow'
-        };
-         try{
-             const response = await fetch(`${host}data/experiments/${exp}/resources/${exp}/files/${file}`, requestOptions)
-             //console.log(response.json())
-             return !!response.ok;
-         } catch (error) {
-             console.log(error)
-         }
+  authenticate_user: async (username, password, host) => {
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
+      },
+      redirect: 'follow',
+    };
+    try {
+      const response = await fetch(`${host}data/JSESSION`, requestOptions);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      } else {
+        return await response.text();
+      }
+    } catch (e) {
+      return (e);
     }
-}
+  },
+  get_all_projects: async (cookie, host) => {
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        cookie: `JSESSIONID=${cookie}`,
+      },
+      redirect: 'follow',
+    };
+    try {
+      const response = await fetch(`${host}data/projects?format=json`, requestOptions);
+      return await response.json();
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  },
+  check_subjects: async (cookie, host, subject) => {
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        cookie: `JSESSIONID=${cookie}`,
+      },
+      redirect: 'follow',
+    };
+    try {
+      const response = await fetch(`${host}data/projects/TRACKFA/subjects/${subject}?format=json`, requestOptions);
+      // console.log(response.json())
+      return !!response.ok;
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  },
+  check_experiments: async (cookie, host, exp, subject) => {
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        cookie: `JSESSIONID=${cookie}`,
+      },
+      redirect: 'follow',
+    };
+    try {
+      const response = await fetch(`${host}data/projects/TRACKFA/subjects/${subject}/experiments/${exp}?format=json`, requestOptions);
+      // console.log(response.json())
+      return !!response.ok;
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  },
+  create_experiment: async (cookie, host, exp, subject, datatype) => {
+    const requestOptions = {
+      method: 'PUT',
+      headers: {
+        cookie: `JSESSIONID=${cookie}`,
+      },
+      redirect: 'follow',
+    };
+    try {
+      const response = await fetch(`${host}data/projects/TRACKFA/subjects/${subject}/experiments/${exp}?xnat:${datatype}/acquisition_site=Monash`, requestOptions);
+      // console.log(response.json())
+      return !!response.ok;
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  },
+  add_resource: async (cookie, host, exp, subject, datatype, file) => {
+    const requestOptions = {
+      method: 'PUT',
+      headers: {
+        cookie: `JSESSIONID=${cookie}`,
+      },
+      redirect: 'follow',
+    };
+    try {
+      const response = await fetch(`${host}data/experiments/${exp}/resources/${exp}/files/${file}`, requestOptions);
+      // console.log(response.json())
+      return !!response.ok;
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  },
+};
